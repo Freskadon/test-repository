343b33ffbe0eff4c3e4c2689aa9d0dbcd66bd8b7
Freskadon <freskadon@gmail.com>
2024-12-17 16:42:43 +0300

    Fix linter and test issues

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   const greeting = "Это тестовый проект Черный Е.А.";
 
-  console.log('This is App ');
+
+
   return (
     <div className="App">
      <header className="App-header">
